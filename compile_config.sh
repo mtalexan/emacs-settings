bfca569d9e4182d03b367aff7aa73a37ca7e140e
Alexander, Michael <mikealexander1860@gmail.com>
2019-01-17 18:23:57 -0800

    try to fix automatic byte compiling not working

--- compile_config.sh
+++ compile_config.sh
@@ -2,26 +2,26 @@
 
 # We have to list the files that can currently be byte-compiled properly
 FILES= \
-     ~/.emacs.d/config/startup/emacs.__loadpath.el \
-     ~/.emacs.d/config/startup/emacs._00preload.el \
-     ~/.emacs.d/config/startup/emacs._02base.el \
-     ~/.emacs.d/config/startup/emacs._03keys.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.cursor-color.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.ediff.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.fileident.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.indent-tabs.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.isearch.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.lock-window.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.smart-beginning-of-line.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.unpop-mark.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.window-split-direction.el \
-     ~/.emacs.d/config/modules/non-packages/emacs.winner.el
+     ./config/startup/emacs.__loadpath.el \
+     ./config/startup/emacs._00preload.el \
+     ./config/startup/emacs._02base.el \
+     ./config/startup/emacs._03keys.el \
+     ./config/modules/non-packages/emacs.cursor-color.el \
+     ./config/modules/non-packages/emacs.ediff.el \
+     ./config/modules/non-packages/emacs.fileident.el \
+     ./config/modules/non-packages/emacs.indent-tabs.el \
+     ./config/modules/non-packages/emacs.isearch.el \
+     ./config/modules/non-packages/emacs.lock-window.el \
+     ./config/modules/non-packages/emacs.smart-beginning-of-line.el \
+     ./config/modules/non-packages/emacs.unpop-mark.el \
+     ./config/modules/non-packages/emacs.window-split-direction.el \
+     ./config/modules/non-packages/emacs.winner.el
 
 # These are used under certain specific conditions, so make sure they're compiled
 FILES+= \
-     ~/.emacs.d/wgrep/wgrep-ag.el \
-     ~/.emacs.d/package23.x/package.el \
-     ~/.emacs.d/config/modules/scripts/guess-style.el
+     ./wgrep/wgrep-ag.el \
+     ./package23.x/package.el \
+     ./config/modules/scripts/guess-style.el
 
 for F in ${FILES} ; do
     # remove any elc files that have already been compiled
